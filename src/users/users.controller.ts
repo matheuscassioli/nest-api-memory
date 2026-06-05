938ba823680f611df1c9d4bc7725fb7a1eaad8d5
Matheus Cassioli <matheuscassioli@hotmail.com>
2026-06-04 22:53:20 -0300

    feature(permissions) : permissao admin e user

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -3,6 +3,8 @@ import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { AuthGuard } from '../auth/auth.guard';
+import { RolesGuard } from '../roles/roles.guard';
+import { Roles } from '../roles/roles.decorator';
 
 @Controller('users')
 
@@ -14,33 +16,35 @@ export class UsersController {
     return this.usersService.create(createUserDto);
   }
 
-  @UseGuards(AuthGuard)
+  @UseGuards(AuthGuard, RolesGuard)
   @Get('test-api')
-  async testExternalApi() {
-    return await this.usersService.fetchDataFromExternalSource();
+  testExternalApi() {
+    return this.usersService.fetchDataFromExternalSource();
   }
 
-  @UseGuards(AuthGuard)
+  @UseGuards(AuthGuard, RolesGuard)
   @Get()
   findAll() {
     return this.usersService.findAll();
   }
 
-  @UseGuards(AuthGuard)
+  @UseGuards(AuthGuard, RolesGuard)
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.usersService.findOne(+id);
   }
-  @UseGuards(AuthGuard)
+
+  @UseGuards(AuthGuard, RolesGuard)
   @Patch(':id')
+  @Roles('admin')
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.usersService.update(+id, updateUserDto);
   }
-  @UseGuards(AuthGuard)
+
+  @UseGuards(AuthGuard, RolesGuard)
   @Delete(':id')
+  @Roles('admin')
   remove(@Param('id') id: string) {
     return this.usersService.remove(+id);
   }
-
-
 }
